bbf13d8591deb58cd4e2bc14bef580eebdf7ec4a
object-Object <object@objectobject.ca>
2023-11-05 14:14:28 -0500

    Run pre-commit in Nox

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,11 @@
 import nox
 
 
-@nox.session
+@nox.session(reuse_venv=True)
 def tests(session: nox.Session):
     session.install("-e", ".[test]", "-e", "./test/_submodules/HexMod")
 
-    session.run("pyright", "src/hexdoc", "--warnings")
+    session.run("pre-commit")
 
     # test cookiecutter last so the extra package install doesn't interfere
     session.run("pytest", *session.posargs)
