c55d8ca4122a8977f3babb55219e3c977fee0ef9
object-Object <object@objectobject.ca>
2023-11-05 14:18:31 -0500

    Actually, don't run pre-commit in nox

--- noxfile.py
+++ noxfile.py
@@ -5,8 +5,7 @@ import nox
 def tests(session: nox.Session):
     session.install("-e", ".[test]", "-e", "./test/_submodules/HexMod")
 
-    session.run("pre-commit")
+    session.run("pytest", *session.posargs)
 
     # test cookiecutter last so the extra package install doesn't interfere
-    session.run("pytest", *session.posargs)
     session.run("pytest", "-k", "test_cookiecutter", "--nox", *session.posargs)
