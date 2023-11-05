2ade7aa1f031a5f94ad8111c2992bf85785d7065
object-Object <object@objectobject.ca>
2023-11-05 14:47:05 -0500

    okay just don't run it then. fine

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,9 @@ import nox
 def tests(session: nox.Session):
     session.install("-e", ".[test]", "-e", "./test/_submodules/HexMod")
 
-    session.run("pytest", *session.posargs)
+    # this apparently CANNOT run from pre-commit in GitHub Actions (venv issues)
+    session.run("pyright", "src", "--warnings")
 
     # test cookiecutter last so the extra package install doesn't interfere
+    session.run("pytest", *session.posargs)
     session.run("pytest", "-k", "test_cookiecutter", "--nox", *session.posargs)
