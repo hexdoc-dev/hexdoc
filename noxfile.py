90c951b29e78dc128ca33e042b96a487115e5f0f
object-Object <object@objectobject.ca>
2023-11-11 18:29:00 -0500

    Move pdoc command into noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,15 +1,18 @@
+from typing import Mapping
+
 import nox
+
+PDOC_FAVICON = "https://github.com/object-Object/hexdoc/raw/main/media/hexdoc.png"
+PDOC_LOGO_LINK = "https://pypi.org/project/hexdoc/"
+PDOC_LOGO = "https://github.com/object-Object/hexdoc/raw/main/media/hexdoc.svg"
+PDOC_EDIT_URL = "https://github.com/object-Object/hexdoc/blob/main/src/hexdoc/"
+
+nox.options.reuse_existing_virtualenvs = True
 
 nox.options.sessions = ["tests"]
 
 
-@nox.session(reuse_venv=True)
-def hexdoc(session: nox.Session):
-    session.install(".")
-    session.run("hexdoc", *session.posargs)
-
-
-@nox.session(reuse_venv=True)
+@nox.session
 def tests(session: nox.Session):
     session.run("pip", "uninstall", "hexdoc-mod", "-y")
     session.install("-e", ".[test]", "-e", "./test/_submodules/HexMod")
@@ -19,3 +22,49 @@ def tests(session: nox.Session):
 
     # test cookiecutter last so the extra package install doesn't interfere
     session.run("pytest", "--nox", *session.posargs)
+
+
+@nox.session
+def hexdoc(session: nox.Session):
+    session.install(".")
+    session.run("hexdoc", *session.posargs)
+
+
+@nox.session
+def pdoc(session: nox.Session):
+    session.install(".[pdoc]")
+
+    version = run_silent(session, "hatch", "version")
+    commit = run_silent(session, "git", "rev-parse", "--short", "HEAD", external=True)
+
+    session.run(
+        "pdoc",
+        "hexdoc",
+        "--favicon",
+        PDOC_FAVICON,
+        "--logo-link",
+        PDOC_LOGO_LINK,
+        "--logo",
+        PDOC_LOGO,
+        "--edit-url",
+        f"hexdoc={PDOC_EDIT_URL}",
+        "--footer-text",
+        f"Version: {version} ({commit})",
+        *session.posargs,
+    )
+
+
+def run_silent(
+    session: nox.Session,
+    *args: str,
+    env: Mapping[str, str] | None = None,
+    external: bool = False,
+):
+    output: str | None = session.run(
+        *args,
+        env=env,
+        silent=True,
+        external=external,
+    )
+    assert output
+    return output.strip()
